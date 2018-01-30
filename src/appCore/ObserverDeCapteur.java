f993dee114362e06cd2636f7ad0816142519652e
TheBirdyDeveloper <tehbirdy3@gmail.com>
2018-01-30 15:19:23 +0100

    Push de l'interface ObserverDeCapteur

---
+++ src/appCore/ObserverDeCapteur.java
@@ -0,0 +1,16 @@
+package appCore;
+
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Interface ObserverDeCapteur.
+ */
+public interface ObserverDeCapteur {
+
+	/**
+	 * Update.
+	 *
+	 * @param subject the subject
+	 */
+	public void update(Capteur subject);
+}
