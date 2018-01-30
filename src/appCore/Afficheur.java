ab072070629911d53b38f60e07335c7c1ccc7399
TheBirdyDeveloper <tehbirdy3@gmail.com>
2018-01-30 15:19:44 +0100

    Push de l'afficheur

---
+++ src/appCore/Afficheur.java
@@ -0,0 +1,13 @@
+package appCore;
+
+import javax.swing.JLabel;
+
+public class Afficheur extends JLabel implements ObserverDeCapteur {
+
+	@Override
+	public void update(Capteur subject) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
